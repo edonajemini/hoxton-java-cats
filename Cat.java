22d1edf00c1cb04f2e6127d277fffabc1ded20c3
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-10-03 13:43:39 +0200

    Changes

--- Cat.java
+++ Cat.java
@@ -3,19 +3,16 @@ public class Cat {
         int age;
         boolean likesCuddles;
 
-// void likescuddles(){
-//     if(likesCuddles == true){
-//         System.out.println(this + "likes cuddles");
-//     }else {
-//         System.out.println(this + "doesnt like cuddles");
-//     }
-// }
     public static void main(String[] args) {
         Cat cat = new Cat();
         cat.name = "Muff";
         cat.age = 2;
-        cat.likesCuddles = true;
-        System.out.println("This is " + cat.name + "who is " + cat.age );
+        if(cat.likesCuddles = true){
+            System.out.println("This is " + cat.name + "who is " + cat.age + "and likes to cuddle");
+        }
+        else{
+            System.out.println("This is " + cat.name + "who is " + cat.age + "and doesnt like to cuddle");
+        }
     }
     
 }
